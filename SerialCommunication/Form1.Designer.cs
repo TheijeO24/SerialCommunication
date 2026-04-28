0172e66637435d9b79463ccedb31798de67d4e24
TheijeO24 <theije.oosterlinck@student.hogent.be>
2026-04-28 16:31:35 +0200

    Titel aangepast

--- SerialCommunication/Form1.Designer.cs
+++ SerialCommunication/Form1.Designer.cs
@@ -873,7 +873,7 @@
             this.Controls.Add(this.comboBoxPoort);
             this.Margin = new System.Windows.Forms.Padding(6);
             this.Name = "Form1";
-            this.Text = "BZL seriële communicatie van Theije OosterlinckV3";
+            this.Text = "BZL seriële communicatie van Theije Oosterlinck";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.tabControl.ResumeLayout(false);
             this.tabPageInstellingen.ResumeLayout(false);
